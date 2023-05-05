9850a2a1223252aa567cc29ee7e37c1256ebd6e3
wwoah <phillipkbishop@gmail.com>
2023-05-05 14:03:57 -0700

    add a few pubs

--- random_iternerary/lib/map_page.dart
+++ random_iternerary/lib/map_page.dart
@@ -1,8 +1,6 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:search_map_place_updated/search_map_place_updated.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -21,17 +19,26 @@ class _MapPageState extends State<MapPage> {
       appBar: AppBar(
         title: const Text('Map'),
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(39.7285, -121.8375), // Set the initial position of the map to Chico
-          zoom: 12.0, // Set the initial zoom level
-        ),
-        markers: markers,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addMarker,
-        child: Icon(Icons.add_location),
+    body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(39.7285, -121.8375), // Set the initial position of the map to Chico
+              zoom: 12.0, // Set the initial zoom level
+            ),
+            markers: markers,
+          ),
+          Positioned(
+            right: 16.0,
+            bottom: 1.0,
+            top: MediaQuery.of(context).size.height / 2 - 50.0,
+            child: FloatingActionButton(
+              onPressed: _addMarker,
+              child: Icon(Icons.add_location),
+            ),
+          ),
+        ],
       ),
     );
   }
