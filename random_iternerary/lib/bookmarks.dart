fbaeaada1c0058e4af3d9664236f52edf745492c
JedJordan <shikirawr@gmail.com>
2023-05-18 14:14:05 -0700

    checkbox print

--- random_iternerary/lib/bookmarks.dart
+++ random_iternerary/lib/bookmarks.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class BookmarksPage extends StatelessWidget {
+class BookmarksPage extends StatefulWidget {
   final String email;
+
   const BookmarksPage({required this.email, Key? key}) : super(key: key);
+
+  @override
+  _BookmarksPageState createState() => _BookmarksPageState();
+}
+
+class _BookmarksPageState extends State<BookmarksPage> {
+  List<String> selectedTitles = [];
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +23,7 @@ class BookmarksPage extends StatelessWidget {
         child: StreamBuilder<QuerySnapshot>(
           stream: FirebaseFirestore.instance
               .collection('users')
-              .where('email', isEqualTo: email)
+              .where('email', isEqualTo: widget.email)
               .snapshots(),
           builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -45,10 +53,21 @@ class BookmarksPage extends StatelessWidget {
                         final bookmarkData = bookmarkDoc.data() as Map<String, dynamic>?;
                         final title = bookmarkData?['title'] as String? ?? 'No Title';
                         final address = bookmarkData?['address'] as String? ?? 'No Address';
+                        final isSelected = selectedTitles.contains(title);
                         return SizedBox(
                           width: double.infinity,
                           child: Card(
-                            child: ListTile(
+                            child: CheckboxListTile(
+                              value: isSelected,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  if (value == true) {
+                                    selectedTitles.add(title);
+                                  } else {
+                                    selectedTitles.remove(title);
+                                  }
+                                });
+                              },
                               title: Text(title),
                               subtitle: Text(address),
                             ),
@@ -62,6 +81,13 @@ class BookmarksPage extends StatelessWidget {
             }
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Print selected titles
+          print(selectedTitles);
+        },
+        child: Icon(Icons.check),
       ),
     );
   }
